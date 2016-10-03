470a9f07caf9a7cccd92351ed7b0ee8ce6dde01b
Jan Gloser <jan.renra.gloser@gmail.com>
2016-10-03 18:40:46 +0200

    Passing test

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe "GET /api/orders", type: :request do
   it "returns orders list" do
     get '/api/orders'
-    expect(status).to eq(200)
-    expect(last_response.body).to eq({ token: 'null' }.to_json)
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to eq({ token: nil, status: 'ok' }.to_json)
   end
 end
