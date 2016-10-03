293c53c00b23c24a047c0144f8c4e3bc0d913393
Jan Gloser <jan.renra.gloser@gmail.com>
2016-10-03 18:37:41 +0200

    Fixing stuff

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 RSpec.describe "GET /api/orders", type: :request do
   it "returns orders list" do
-    binding.pry
     get '/api/orders'
     expect(status).to eq(200)
     expect(last_response.body).to eq({ token: 'null' }.to_json)
